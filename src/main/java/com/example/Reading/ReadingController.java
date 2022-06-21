d6cbe10356ac7aa810130e84d0b89ec0737d6901
SpenserMG <spensermgreeley@gmail.com>
2022-06-21 09:30:53 -0400

    Strong tag was missing closing bracket

--- src/main/java/com/example/Reading/ReadingController.java
+++ src/main/java/com/example/Reading/ReadingController.java
@@ -34,7 +34,7 @@ public class ReadingController
                 // The modifiedWord variable is taking in each character with HTML bold tags at position i
                 modifiedWord +=  word.charAt(i);
             }
-            modifiedWord = "<strong>" + modifiedWord + "</strong";
+            modifiedWord = "<strong>" + modifiedWord + "</strong>";
 
             // Adds in the unbolded portion of each word onto the end of the bolded portion to make a complete word
             modifiedWord += word.substring(wordNumber);
